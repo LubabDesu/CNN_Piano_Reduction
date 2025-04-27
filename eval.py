2db3472b279b264be9227175e440f5c748d546b7
Lucas Yan <lucasyanyikang88@gmail.com>
2025-04-26 21:06:57 -0700

    eval.py

--- eval.py
+++ eval.py
@@ -5,6 +5,10 @@ from main import test_dataset, test_loader
 from data import PianoReductionDataset
 from torch.utils.data import DataLoader
 
+#Helper function to binarize outputs
+def binarize(output_list) :
+    return (output_list > 0.5) * 1 
+
 # Load model and set to evaluation mode
 model = PianoReductionCNN()
 model.load_state_dict(torch.load("first-train-model.pth"))
@@ -23,6 +27,8 @@ print("Total weight sum:", total)
 
 
 # load test set (already loaded in main)
+all_generated_outputs = []
+all_groundtruth_outputs = []
 
 #Eval loop
 with torch.no_grad():
@@ -31,10 +37,26 @@ with torch.no_grad():
         batch_targets = batch['target'].to(device).permute(0, 3, 1, 2)
 
         #Check after each conv
-        x = model.down_conv1(batch_inputs)
-        print("After conv1:", torch.sum(x))
-        print("sum of inputs : " + str(torch.sum(batch_inputs)))
+        # x = model.down_conv1(batch_inputs)
+        # print("After conv1:", torch.sum(x))
+        # print("sum of inputs : " + str(torch.sum(batch_inputs)))
+        # outputs = model(batch_inputs)
+        # print(outputs)
+        # print(outputs.shape)
+        # print("sum of outputs now" + str(torch.sum(outputs)))
+
         outputs = model(batch_inputs)
-        print(outputs)
-        print(outputs.shape)
-        print("sum of outputs now" + str(torch.sum(outputs)))
+
+        all_generated_outputs.append(outputs.cpu())
+        all_groundtruth_outputs.append(batch_targets.cpu())
+
+all_generated_outputs = torch.cat(all_generated_outputs, dim=0)
+all_groundtruth_outputs = torch.cat(all_groundtruth_outputs, dim=0)
+
+#Binarize the outputs 
+all_generated_outputs = binarize(all_generated_outputs)
+all_groundtruth_outputs = binarize(all_groundtruth_outputs)
+
+active_indices = torch.nonzero(all_generated_outputs)
+
+print("active indices are : ", active_indices)
